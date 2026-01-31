cc199a5c3dbea6ae6a702cf501f762aeee41506f
hashijun72 <243115@kcsk.onmicrosoft.com>
2026-01-31 23:27:34 +0900

    英語対応 (#5)

---
+++ src/english.js
@@ -0,0 +1,91 @@
+/**
+ * English translations for data-i18n keys.
+ * index.html uses data-i18n="key" and this object provides the English text.
+ */
+export const en = {
+  // Aria labels
+  'aria.themeToggle': 'Toggle theme',
+  'aria.langToggle': '日本語 / English',
+
+  // Language button label (shows current language name)
+  'lang.label': 'English',
+
+  // Hero
+  'hero.name': 'Amai Natuki',
+  'hero.subtitle': '✨ Student / Developer / Writer / Photographer ✨',
+
+  // Profile
+  'profile.sectionTitle': 'About Me',
+  'profile.intro':
+    'Hi! I\'m <strong>Amai Natuki</strong>.<br>\n' +
+    'I\'m a student who loves programming and VRChat ✨<br>\n' +
+    'I write articles on vrnabi and make handy tools!<br>\n' +
+    '<br>\n' +
+    'My motto is "<strong>Enjoy personal development</strong>" and "<strong>Solve problems with technology</strong>".',
+  'profile.badgeVrnabi': 'vrnabi Operator',
+  'profile.badgeMofu': 'MofuMagic Owner',
+  'profile.badgePhoto': 'VRChat Photographer',
+
+  // Projects section
+  'projects.sectionTitle': 'Projects',
+
+  // Project cards & modal descriptions
+  'project.vrnavi.title': 'vrnabi',
+  'project.vrnavi.desc': 'A site that gathers VRChat information',
+  'project.vrnavi.description':
+    'A site that gathers VRChat information. We share event info, world introductions, and more to help you enjoy VRChat.<br><br>You can try the embedded page below.',
+
+  'project.facemixer.title': 'FaceMixer',
+  'project.facemixer.desc': 'Easy VRChat avatar expression editing!',
+  'project.facemixer.description':
+    'Make VRChat avatar expression editing easy! An intuitive tool to create and edit expressions in Unity.',
+
+  'project.cliprack.title': 'ClipRack',
+  'project.cliprack.desc': 'Clipboard manager app for macOS',
+  'project.cliprack.description':
+    'An app that adds clipboard history so copy-paste is more convenient.',
+
+  'project.booth-import.title': 'BOOTH Import Assistant',
+  'project.booth-import.desc': 'Makes importing from BOOTH easier',
+  'project.booth-import.description':
+    'A tool that makes importing from BOOTH easier. It streamlines importing purchased avatars and outfits. You need to install a browser extension to use it.',
+
+  'project.booth-library.title': 'BOOTH Library Search',
+  'project.booth-library.desc': 'Quick search for your BOOTH library',
+  'project.booth-library.description':
+    'A Chrome extension that lets you search your BOOTH library quickly. Smooth search from purchase history.',
+
+  'project.360-viewer.title': '360-viewer',
+  'project.360-viewer.desc': '360° image viewer in the browser',
+  'project.360-viewer.description':
+    'A 360° image viewer in the browser. You can browse while interacting to experience the appeal of the metaverse.<br><br>You can try the embedded page below.',
+
+  'project.timecard.title': 'TimeCard Web',
+  'project.timecard.desc': 'Attendance management site (school project)',
+  'project.timecard.description':
+    'An attendance management site created as a school project. Built with Java Servlets on Apache Tomcat. Runs on a home server.',
+
+  'project.toc-ad.title': 'TOC-AD',
+  'project.toc-ad.desc': 'WordPress plugin that adds ad images under the table of contents',
+  'project.toc-ad.description':
+    'A WordPress plugin that automatically adds ad images below the table of contents.',
+
+  // Gallery
+  'gallery.sectionTitle': 'Gallery',
+  'gallery.desc': 'Memories from VRChat!',
+  'gallery.caption1': 'Summer ☀️',
+  'gallery.caption2': 'JPT Party 🎉',
+  'gallery.caption3': 'Beautiful View ✨',
+  'gallery.caption4': 'Street Snap 📸',
+  'gallery.caption5': 'Memories...',
+  'gallery.caption6': 'Cute!',
+
+  // Links (labels are often kept as-is; add if you want different wording)
+  'links.twitter': 'X(Twitter)',
+  'links.twitch': 'Twitch',
+  'links.booth': 'BOOTH',
+  'links.github': 'GitHub',
+
+  // Footer
+  'footer.text': 'Made with by Amai Natuki',
+};
